5582549103238dcf81a01fdf8379e0deb45462ce
Segwegler <-NA->
2021-07-30 12:50:31 -0800

    updated listening port
    The port that node listens to can be set with an environmental variable

--- index.js
+++ index.js
@@ -305,8 +305,9 @@ app.use((err, req, res, next) => {
   res.status(500).send("somthing broke!");
 });
 
+//set port to either the specified enviernment variable or 8080
+const port = process.env.PORT || 8080;
 
-
-app.listen(8080, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log("Listening on port 8080");
 });
